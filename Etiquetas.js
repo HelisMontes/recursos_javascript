2c726d2f350d008e892856b69f06e916a9aecc75
HelisMontes <helismonntes@gmail.com>
2020-06-02 19:30:04 -0500

    Animacion Galeria

---
+++ Etiquetas.js
@@ -0,0 +1,38 @@
+appendChild()           /*ETIQUETA ES UN MÉTODO QUE SE USA PARA CREAR NUEVOS ELEMENTOS EN EL DOM*/
+createElement()         /*ETIQUETA ES OTRO MÉTODO Y DENTRO VA LA ETIQUETA HTML QUE SE VA A CREAR*/
+setAttribute            /*ETIQUETA ES PARA AGREGAR UN ATRIBUTO A UNA ETIQUETA HTML EN EL DOM, PRIMERO SE DEBE PONER EL ATRINUTO Y LUEGO EL VALOR*/
+
+                        /*appendChild(document.createElement("DIV")).setAttribute("id", "lighbox")*/
+
+
+outerHTMl               /*ETIQUETA  ES PARA TREAR EL CONTENIDO HTML Y NO EL OBJETO*/
+innerHTML               /* Etiqueta para obtener, agregar o remplazar contenido HTML*/
+
+                        /*pg.modal.innerHTML = imgRuta.outerHTML+"<div>x</div> ";*/
+
+childNodes              /*ETIQUETA MUESTRA LOS HIJOS DE LA EIQUETA*/
+                    
+                        /*  <div id="lighbox"></div>
+                            let cerrar = pg.modal.childNodes[1];
+                            let imagen= pg.modal.childNodes[0];
+                        */
+
+parentNode              /*  MUESTRA AL PADRE DE LA EIQUETA  */
+removeChild             /*  ETIQUETA RUMUEVE LA ETIQUETA DEL HIJO   */
+                        
+                        /*  <div id="lighbox"></div>        */
+                        /*  pg.lighbox.parentNode.removeChild(pg.lighbox);    */   
+
+
+
+target.getAttribute()   /*  ES UNA FUNCIÓN PARA SELECCIONAR CUALQUIER ATRIBUTO QUE TENGA LA ETIQUETA INDICADA EN EL PARÁMETRO ("X")*/
+
+                        /*  tecla.target.getAttribute("class"); */
+
+eval(p.operaciones.innerHTML)   /*  eval () es una funcion predeterminada de javascript para resolver funciones matematicas de una variable */
+
+
+
+
+
+// pg.lighbox.style = "width: 100%; height: 100%; position: fixed; zIndex: 10; background: rgba(0,0,0,0.7); top: 0; left: 0;"
--- js/galeria.js
+++ js/galeria.js
@@ -8,7 +8,9 @@ let pg = {
     rutaImagen: null,
     cuerpoDom : document.querySelector("body"),
     lighbox: null,
-    modal: null
+    modal: null,
+    animacionGaleria: "fade"
+
 
 }
 
@@ -38,13 +40,7 @@ let mg ={
     },
 
     lighbox: function(imgRuta){
-
-        /*Etiqueta appendChild() es un metodo que se usa para crear nuevos elementos en el DOM*/
-        /*Etiqueta createElement() es otro metodo y dentro va la etiqueta HTML que se va a crear*/
-        /*Etiqueta setAttribute es para agregar un atributo a una etiqueta HTML en el DOM, primero se debe poner el atrinuto y luego el valor*/
-        /*Etiqueta outerHTMl es para trear el contenido HTML y no el objeto*/
-        /*Etiqueta childNodes MUESTRA LOS HIJOS DE LA EIQUETA*/
-        
+       
         pg.cuerpoDom.appendChild(document.createElement("DIV")).setAttribute("id", "lighbox")
         pg.lighbox = document.querySelector("#lighbox");
     
@@ -55,43 +51,116 @@ let mg ={
         pg.lighbox.style.background = "rgba(0,0,0,0.7)";
         pg.lighbox.style.top = "0";
         pg.lighbox.style.left = "0"; 
-
-       // pg.lighbox.style = "width: 100%; height: 100%; position: fixed; zIndex: 10; background: rgba(0,0,0,0.7); top: 0; left: 0;"
-        
+               
         pg.lighbox.appendChild(document.createElement("DIV")).setAttribute("id", "modal");
         pg.modal = document.querySelector("#modal");
 
-        pg.modal.innerHTML = imgRuta.outerHTML+"<div>X</div> ";
+        pg.modal.innerHTML = imgRuta.outerHTML+"<div>x</div> ";
 
-        let imagen = pg.modal.childNodes[0];
         let cerrar = pg.modal.childNodes[1];
-        let margin_left = 0;
-        let margin_top = 0;
+        let imagen= pg.modal.childNodes[0];
 
-        margin_left = imagen.width;
-        margin_top = imagen.height;
+        pg.modal.style.display = "block";
+        pg.modal.style.position = "relative";
+        imagen.style.width  = "100%";
+        imagen.style.border  = "15px solid white";
+      
+        if(window.matchMedia("(max-width:1000px)").matches){
 
-        console.log("marginLeft", margin_left);
-        console.log("marginTop", margin_top);
+            pg.modal.style.width = "90%";
+
+        }else{
+
+            pg.modal.style.width = "60%";
+
+        }
 
         Object.assign(pg.modal.style,{
-     
-            display: "block", 
-            position: "relative",
-            width: "60%",
-            top:  "50%",
+
+            top: "50%",
             left: "50%",
-            marginLeft: -margin_left/2+"px",
-            marginTop:  -margin_top/2+"px",
-           
-        });
+            marginLeft: -imagen.width/2+"px",
+            marginTop: -imagen.height/2+"px"
         
-        Object.assign(imagen.style,{
+        });
 
-            width: "100%",
-            border: "15px solid white"
+        if(pg.animacionGaleria == "slideLeft"){
 
-        })
+            Object.assign(pg.modal.style,{
+
+                top: "50%",
+                left: "0",
+                opacity: "0"
+                
+            });
+
+            setTimeout(function(){
+                
+                Object.assign(pg.modal.style,{
+
+                    transition: ".5s left ease",
+                    left: "50%",
+                    opacity: "1",
+                    marginLeft: -imagen.width/2+"px",
+                    marginTop: -imagen.height/2+"px"
+                    
+                })
+
+            },50);
+
+        }
+
+        if(pg.animacionGaleria == "slideTop"){
+
+            Object.assign(pg.modal.style,{
+
+                top: "-100%",
+                left: "50%",
+                opacity: "0"
+                
+            });
+
+            setTimeout(function(){
+                
+                Object.assign(pg.modal.style,{
+
+                    transition: ".5s top ease",
+                    top: "50%",
+                    opacity: "1",
+                    marginLeft: -imagen.width/2+"px",
+                    marginTop: -imagen.height/2+"px"
+                    
+                })
+
+            },50);
+
+        }
+
+        if(pg.animacionGaleria == "fade"){
+
+            Object.assign(pg.modal.style,{
+
+                top: "50%",
+                left: "50%",
+                opacity: "0"
+                
+            });
+
+            setTimeout(function(){
+                
+                Object.assign(pg.modal.style,{
+
+                    transition: ".5s opacity ease",
+                    top: "50%",
+                    opacity: "1",
+                    marginLeft: -imagen.width/2+"px",
+                    marginTop: -imagen.height/2+"px"
+                    
+                })
+
+            },50);
+
+        }
 
         Object.assign(cerrar.style,{
 
@@ -108,7 +177,33 @@ let mg ={
             borderRadius: "0px 0px 0px  5px" 
 
         });
+        
+        cerrar.addEventListener("click", mg.salirGaleria);
+        document.addEventListener("keydown",this.salirGaleria);
+    },
+
+    salirGaleria: function(e){
+      
+        console.log("valorde de E", e.type);
+      
+        if(e.type == "click"){
+           
+            pg.lighbox.parentNode.removeChild(pg.lighbox);
+
+        }else{
+        
+            let valorCode = e.keyCode;
+                        
+            if(valorCode == 27 && pg.lighbox.getAttribute("id") == "lighbox"){
                 
+                console.log("lighbox", pg.lighbox);
+                
+                pg.lighbox.parentNode.removeChild(pg.lighbox);
+                pg.lighbox.setAttribute("id", "")
+                
+            }
+
+        }
 
     }
 
