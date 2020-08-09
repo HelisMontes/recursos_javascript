dab0057c0b9a55923c4e5326de8941e250b22ac7
Helis Montes <helismontes@gmail.com>
2020-08-08 20:18:21 -0500

    Actualizacion

--- Etiquetas.js
+++ Etiquetas.js
@@ -37,4 +37,27 @@ appendChild             /*  Agrega un nuevo nodo al final de la lista de un elem
                         /*  ul.appendChild(li) */
 
 
-// pg.lighbox.style = "width: 100%; height: 100%; position: fixed; zIndex: 10; background: rgba(0,0,0,0.7); top: 0; left: 0;"
+ pg.lighbox.style = "width: 100%; height: 100%; position: fixed; zIndex: 10; background: rgba(0,0,0,0.7); top: 0; left: 0;"
+
+ `
+ col-md-12 col-lg-12 col-xl-12 col-sm-6 col-xs-12
+
+ col-xl ETIQUETAS PARA PANTALLAS EXTRAGRANDE 1360
+ col-lg ETIQUETAS PARA PANTALLAS TABLET HORIZONTAL 1024
+ col-md ETIQUETAS PARA PANTALLAS TABLET VERTICALES 769
+ col-sm-6 ETIQUETAS PARA PANTALLAS MOVIL HORIZONTAL 769
+ col-xs-12 ETIQUETAS PARA PANTALLAS MOVIL VERTICALES 769
+ 
+EL VALOR MAXIMO ES DE 12 SI SE DIVIDE EN VARIAS SESSCIONES, SE DEBE IR JUGANDO CON EL TAMAÑO
+
+<div class="row border my-3 bg-white">
+				
+				<figure class="col-md-5 col-lg-4 col-xl-3 p-2 mb-0">
+					
+    			</figure>
+
+				<div class="col-md-7 col-lg-8 col-xl-9 p-4">
+
+                </div>
+`
+ 
